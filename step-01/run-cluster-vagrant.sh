9e660c96546c9a165cfc99aeb2c09b6e02254703
Cedric Hauber <cedric.hauber@wescale.fr>
2015-12-07 14:34:36 +0100

    IpS

--- step-01/run-cluster-vagrant.sh
+++ step-01/run-cluster-vagrant.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-proxy=http://10.2.0.4:3128
-export http_proxy=$proxy
-export https_proxy=$proxy
-export HTTP_PROXY=$proxy
-export HTTPS_PROXY=$proxy
 cd ../kubernetes-vagrant-coreos-cluster/
 vagrant up
 cd ../step-01
